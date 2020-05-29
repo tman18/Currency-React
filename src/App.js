36fab8195abf25fbc31eebd9813beb114dbe4714
Tim Myers <tim.myers@jewsforjesus.org>
2020-05-28 20:46:43 -0700

    final tweaks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-// function render() {
-//     let chart = document.querySelector("#graph-location");
-//     chart.innerHTML= ""
-//     for (let rate of apiData) {
-//         if (displayCountries.includes(rate[0])) {
-//           conversion = rate[1].toFixed(2)
-//           let height = 50 / conversion
-//           let country = rate[0]
-//           chart.innerHTML += `
-//             <div class="graph-data-bar" style="height: ${height}%" onClick="alert('1 USD = ${conversion} ${country}')">
-//               <div class="graph-data-bar-country-name">
-//                 ${country}
-//               </div>
-//             </div>  
-//         `;
-//         };
-//     }
-
-// }
 
 
 function App() {
@@ -29,13 +8,11 @@ function App() {
   function fetchAPIData() {
     let dropdown = document.querySelector('#date-dropdown');
     let year = dropdown.options[dropdown.selectedIndex].value;
-    console.log(year)
     fetch(`https://api.exchangeratesapi.io/${year}-01-01?base=USD`)
     .then(response => response.json())
     .then(data => {
     const newData = Object.entries(data.rates);
     newData.sort()
-    console.log("newData:", newData)
     setApiData(newData)
     });
   }
@@ -44,8 +21,6 @@ function App() {
 
   function calculateBarHeight(exchangeRate) {
     let height = 50 / exchangeRate
-    console.log(exchangeRate)
-    console.log(height)
     return height
   }
 
